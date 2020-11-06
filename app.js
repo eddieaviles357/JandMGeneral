0f48a64b9dc6da9bb31b68704632ff765d1feaaf
Eduardo Aviles <eduardoaviles357@yahoo.com>
2020-11-05 23:24:37 -0800

    added var in .dotenv and .gitignore

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-// import express from "express";
-const { createSecretKey } = require("crypto");
-const express = require("express");
+
+const dotenv = require("dotenv");
 const path = require("path");
 
+const express = require("express");
+const { json } = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+// load env variables
+dotenv.config({ path: "./config/config.env" });
 
+// body parser
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/public")));
+// use static files
+app.use(express.static(path.join(__dirname, "./public")));
+
 
 app.get('/', (req, res) => {
   // res.render("index.html");
-  const message = "message send";
-  console.log(req)
   // res.sendFile()
+  res.end();
 })
 
-app.post('/', (req, res) => {
+app.post('/contact', (req, res) => {
+  res.setHeader("content-type", "application/json").json(req.body);
   console.log(req.body)
-  res.redirect("/");
-  
 })
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
-});
+const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`App up in ${process.env.NODE_ENV} listening on port ${PORT}!`);
+// });
+const server = app.listen(
+  PORT, () => 
+  console.log(`server up in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  );
+  
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+})
