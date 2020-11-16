0463c9ebe495523f7142c5fed40498d1aadea7ae
Eduardo Aviles <eduardoaviles357@yahoo.com>
2020-11-16 15:57:32 -0800

    installed EJS, seperated html components to ejs views, updated some scss files, added dynamic rendering of html views

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
-const dotenv = require("dotenv");
-const path = require("path");
-
-const express = require("express");
-const { isEmail, isAlpha } = require('validator'); // to validate user input
-
-const app = express();
+const dotenv = require("dotenv"),
+      path = require("path"),
+      express = require("express"),
+      app = express();
+const { isEmail, isAlpha } = require("validator"); // Validate user input
 
 // load env variables
 dotenv.config({ path: "./config/config.env" });
 
 // body parser
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.urlencoded({ extended: true }))
+
 // use static files
 app.use(express.static(__dirname + "/public"));
-console.log(path.basename(__dirname));
 
+// view engine
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+// routes
 app.get('/',function(req,res){
-  res.sendFile("/index.html");
+  let submit = false;
+  res.render("index", { submit: submit });
 });
 
 app.post('/', (req, res) => {
-  const { name, email, comment } = req.body;
-  if(isEmail(email) && isAlpha(name, 'en-US'))
-  { console.log('yes')}
- 
-  console.log(`Name: ${name}, Comment: ${comment}, Email: ${email}`);
-  res.redirect(301, '/');
+  // initialize to strings for validation to work
+  let name = '', // no numbers allowed
+      email = '', 
+      comment = '';
+  // let submit = false;
+
+  // concatinate user input
+  name += req.body.name;
+  email += req.body.email;
+  comment += req.body.comment;
+
+  console.log(
+    `Name is: ${typeof name}: ${name},
+    Email is: ${typeof email}: ${email},
+    Comment is: ${typeof comment}: ${comment}`
+    );
+
+  if(isEmail(email) && isAlpha(name, "en-US"))
+  { 
+    submit = true;
+    console.log("yes");
+  }
+  res.render("index", { submit : submit })
 })
+
+// end of routes
+
 
 const PORT = process.env.PORT || 3000;
 
