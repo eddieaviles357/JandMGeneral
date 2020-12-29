1fc9de9bf8e21e08d89cd8409209337a390422ae
eddieaviles357 <eduardoaviles357@yahoo.com>
2020-12-29 15:54:52 -0800

    completed footer, changed testimonial

--- public/src/components/nav.js
+++ public/src/components/nav.js
@@ -1,10 +1,10 @@
 let navToggle = document.querySelector(".nav-toggle");
 let navList = document.querySelector(".nav__list");
-let title = document.querySelector(".title");
+// let title = document.querySelector(".title");
 let header = document.querySelector(".header");
 
 
 navToggle.addEventListener("click", e => {
     navList.classList.toggle("hamburger");
-    title.classList.toggle("hamburger-title");
+    // title.classList.toggle("hamburger-title");
 })
