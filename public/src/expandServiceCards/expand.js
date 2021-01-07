89a42b27b0d1189aabc30d742572c1b3626f640d
eddieaviles357 <eduardoaviles357@yahoo.com>
2021-01-06 20:53:18 -0800

    small changes to expand to include positive and negative indication

--- public/src/expandServiceCards/expand.js
+++ public/src/expandServiceCards/expand.js
@@ -1,21 +1,24 @@
-let card1 = document.getElementsByClassName("card__details--1");
-let card2 = document.getElementsByClassName("card__details--2");
-let card3 = document.getElementsByClassName("card__details--3");
+let card1 = document.querySelector(".card__details--1");
+let card2 = document.querySelector(".card__details--2");
+let card3 = document.querySelector(".card__details--3");
 
 let buttonExpand1 = document.getElementById("expandBtn--1");
 let buttonExpand2 = document.getElementById("expandBtn--2");
 let buttonExpand3 = document.getElementById("expandBtn--3");
 
 let toggleExpand1 = (_) => {
-  card1[0].classList.toggle("expand--1");
+  card1.classList.toggle("expand--1");
+  (card1.classList.length === 3) ? buttonExpand1.innerHTML = "-" : buttonExpand1.innerHTML = "+";
 }
 
 let toggleExpand2 = (_) => {
-  card2[0].classList.toggle("expand--2");
+  card2.classList.toggle("expand--2");
+  (card2.classList.length === 3) ? buttonExpand2.innerHTML = "-" : buttonExpand2.innerHTML = "+";
 }
 
 let toggleExpand3 = (_) => {
-  card3[0].classList.toggle("expand--3");
+  card3.classList.toggle("expand--3");
+  (card3.classList.length === 3) ? buttonExpand3.innerHTML = "-" : buttonExpand3.innerHTML = "+";
 }
 
 buttonExpand1.addEventListener("click", toggleExpand1);
